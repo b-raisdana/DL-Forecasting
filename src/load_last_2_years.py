ed61561182d1cd8e70f706186fdf83a7b78c55bb
brais <b.raisdana@gmail.com>
2024-09-27 14:26:44 +0330

    Completed: preprocessing load_last_2_years

---
+++ src/load_last_2_years.py
@@ -0,0 +1,14 @@
+import os
+
+from datetime import timedelta
+
+import pandas as pd
+
+from Config import config
+from Model.Data.atr import read_multi_timeframe_ohlcva
+from helper.helper import date_range_to_string
+
+for end in pd.date_range(pd.to_datetime('08-01-22'), pd.to_datetime('09-01-24'), freq=timedelta(days=30)):
+    config.processing_date_range = date_range_to_string(start=end - timedelta(days=60),
+                                                        end=end)
+    read_multi_timeframe_ohlcva(config.processing_date_range)
