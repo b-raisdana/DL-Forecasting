7f428141742122249d22102630c09045e12d9d1f
brais <b.raisdana@gmail.com>
2024-10-03 18:42:41 +0330

    Completed: config.path_of_data replaced with data_path()

--- src/load_last_2_years.py
+++ src/load_last_2_years.py
@@ -9,5 +9,5 @@ from helper.helper import date_range_to_string
 for end in pd.date_range(pd.to_datetime('08-01-22'), pd.to_datetime('09-01-24'), freq=timedelta(days=30)):
     config.processing_date_range = date_range_to_string(start=end - timedelta(days=60),
                                                         end=end)
-    config.under_process_symbol = 'SOLUSDT'
+    config.under_process_symbol = 'BNBUSDT'
     read_multi_timeframe_ohlcv(config.processing_date_range)
