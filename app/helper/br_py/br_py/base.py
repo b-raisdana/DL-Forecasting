7060d8af2576af5435dd3fb8db3f0cde342230d5
brais <b.raisdana@parshan.net>
2025-03-28 11:40:15 +0330

    tf-wsl

--- app/helper/br_py/br_py/base.py
+++ app/helper/br_py/br_py/base.py
@@ -3,14 +3,14 @@ import asyncio
 from .do_log.log_it import init_logger
 from .profiling.base import init_global_profile_to_db
 
-#
 
 async def br_lib_init(path_of_logs: str, log_to_std_out_level: int, log_to_file_level: int,
-                      root_path: str, global_profile_to_db: bool = False, file_log_rotation_size = "100 MB", file_log_retention_duration = "30 days"):
+                      root_path: str, global_profile_to_db: bool = False, file_log_rotation_size="100 MB",
+                      file_log_retention_duration="30 days"):
     init_logger(path_of_logs=path_of_logs, log_to_std_out_level=log_to_std_out_level,
                 log_to_file_level=log_to_file_level, root_path=root_path,
-                file_log_rotation_size = file_log_rotation_size,
-                file_log_retention_duration = file_log_retention_duration)
+                file_log_rotation_size=file_log_rotation_size,
+                file_log_retention_duration=file_log_retention_duration)
     if global_profile_to_db:
         init_global_profile_to_db()
 
@@ -19,4 +19,3 @@ def sync_br_lib_init(path_of_logs: str, log_to_std_out_level: int, log_to_file_l
                      root_path: str, global_profile_to_db: bool = False):
     asyncio.run(br_lib_init(path_of_logs=path_of_logs, root_path=root_path, log_to_file_level=log_to_file_level,
                             log_to_std_out_level=log_to_std_out_level))
-
