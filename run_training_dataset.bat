7f0c35e0cf91cc8129ba083cb4972f29ae2c79f8
brais <b.raisdana@parshan.net>
2025-04-18 02:06:25 +0330

    nan has being fixed.

---
+++ run_training_dataset.bat
@@ -0,0 +1,3 @@
+@echo off
+set PYTHONPATH=C:\Code\DL-Forecasting\app
+C:\Code\DL-Forecasting\.venv\Scripts\python.exe C:\Code\DL-Forecasting\app\ai_modelling\cnn_lstm_mt_indicators_to_profit_loss\training_datasets.py
